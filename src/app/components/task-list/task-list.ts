ec50eae78542a29ac0a05d53b04cc456fde2f9f5
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-01 16:31:17 +0100

    Add css to both task-detail and task-list and improve explanations on both components html.

--- src/app/components/task-list/task-list.ts
+++ src/app/components/task-list/task-list.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { TaskDetail } from '../task-detail/task-detail';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [TaskDetail],
+  imports: [TaskDetail, JsonPipe],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
@@ -13,7 +14,7 @@ export class TaskList {
     { id: 2, title: 'Task 2', description: 'Description for Task 2' },
     { id: 3, title: 'Task 3', description: 'Description for Task 3' },
   ]);
-
+  // Signal = reactive state container (no RxJS needed)
   selectedTask = signal<{ id: number; title: string } | null>(null);
 
   selectTask(task: { id: number; title: string }) {

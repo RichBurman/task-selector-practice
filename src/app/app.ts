a05381cdaa0f527dc95885400af5e0cf99258ba5
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-01 15:38:43 +0100

    Create for loop of tasks in task list and render to browser

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskList } from './components/task-list/task-list';
+import { TaskDetail } from './components/task-detail/task-detail';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskList, TaskDetail],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

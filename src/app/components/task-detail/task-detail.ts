0a61a3e517eed0763f6902bd5103946aa544fee7
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-01 15:52:03 +0100

    Add Input/Output and EventEmitter to task-detail and connect task and clearTask between parent and child

--- src/app/components/task-detail/task-detail.ts
+++ src/app/components/task-detail/task-detail.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-task-detail',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './task-detail.css',
 })
 export class TaskDetail {
+  // Input -> this is data from parent! 
+  @Input() task: { id: number; title: string } | null = null;
+  // Output -> sending this event to the parent! 
+  @Output() clearTask = new EventEmitter<void>();
 
+  clearSelectedTask() {
+    this.clearTask.emit();
+  }
 }
